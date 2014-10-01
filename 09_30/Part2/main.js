d87b2998b074f36cccba054b3e1b6af9d01682c9
Ken McLeod <admin@therinken.com>
2014-10-01 07:53:28 -0500

    wrapping up some homework

--- 09_30/Part2/main.js
+++ 09_30/Part2/main.js
@@ -4,8 +4,9 @@ var matt=[240, 210.4, 235.1, 208.9, 197.5], thisTotal=0,thisAverage=0;
   {thisTotal+=matt[i];}
 // calculate average
  thisAverage=(thisTotal/matt.length);
-// display result
+ // display result
  console.log("Matt's overall marathon average is " + thisAverage.toFixed(2) + " minutes!");
+
 
  var mark=[120, 110.4, 135.1, 108.9, 97.5], thisTotal=0,thisAverage=0;
 // add elements of array together
@@ -13,5 +14,4 @@ var matt=[240, 210.4, 235.1, 208.9, 197.5], thisTotal=0,thisAverage=0;
   {thisTotal+=mark[i];}
 // calculate average
  thisAverage=(thisTotal/mark.length);
-// display result
- console.log("Mark's overall marathon average is " + thisAverage.toFixed(2) + " minutes!");
+console.log("Mark's overall marathon average is " + thisAverage.toFixed(2) + " minutes!");
